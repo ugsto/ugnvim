5fc3ccbfe876d3b22db5fef2632fd6682e2522e8
André Augusto Bortoli <andre@bortoli.phd>
2025-04-07 20:46:14 -0300

    feat: add keymaps

--- config/default.nix
+++ config/default.nix
@@ -6,4 +6,27 @@
     ./mini.nix
     ./snacks.nix
   ];
+
+  keymaps = [
+    {
+      action = "<cmd>w<cr>";
+      key = "<C-s>";
+    }
+    {
+      action = "<cmd>q<cr>";
+      key = "<C-q>";
+    }
+    {
+      action = "<cmd>bnext<cr>";
+      key = "<tab>";
+    }
+    {
+      action = "<cmd>bprevious<cr>";
+      key = "<S-tab>";
+    }
+    {
+      action = "<cmd>bdelete<cr>";
+      key = "<leader>bd";
+    }
+  ];
 }
