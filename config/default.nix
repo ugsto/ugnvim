2f39f1262a309db0c97feec79bc2ea2ba2911fb1
André Augusto Bortoli <andre@bortoli.phd>
2025-04-07 23:20:39 -0300

    refactor: split keymaps

--- config/default.nix
+++ config/default.nix
@@ -8,27 +8,4 @@
     ./modules/plugin/utils/snacks.nix
     ./options.nix
   ];
-
-  keymaps = [
-    {
-      action = "<cmd>w<cr>";
-      key = "<C-s>";
-    }
-    {
-      action = "<cmd>q<cr>";
-      key = "<C-q>";
-    }
-    {
-      action = "<cmd>bnext<cr>";
-      key = "<tab>";
-    }
-    {
-      action = "<cmd>bprevious<cr>";
-      key = "<S-tab>";
-    }
-    {
-      action = "<cmd>bdelete<cr>";
-      key = "<leader>bd";
-    }
-  ];
 }
