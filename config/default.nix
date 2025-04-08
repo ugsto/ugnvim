c105826970b0dc9c003e96dfe29c173c6cffebf7
André Augusto Bortoli <andre@bortoli.phd>
2025-04-07 23:35:18 -0300

    feat: add treesitter

--- config/default.nix
+++ config/default.nix
@@ -4,6 +4,7 @@
     ./modules/colorscheme.nix
     ./modules/plugin/lsp/conform.nix
     ./modules/plugin/lsp/lsp.nix
+    ./modules/plugin/lsp/treesitter.nix
     ./modules/plugin/utils/gitsigns.nix
     ./modules/plugin/utils/mini.nix
     ./modules/plugin/utils/snacks.nix
