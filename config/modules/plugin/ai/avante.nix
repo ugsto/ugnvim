dd678d0283122f4aa858be15d0a177f88359589b
André Augusto Bortoli <andre@bortoli.phd>
2025-04-12 22:06:46 -0300

    feat: config avante

--- config/modules/plugin/ai/avante.nix
+++ config/modules/plugin/ai/avante.nix
@@ -1,3 +1,16 @@
 {
-  plugins.avante.enable = true;
+  plugins.avante = {
+    enable = true;
+    settings = {
+      provider = "openai";
+      openai = {
+        endpoint = "https://localhost:11434/v1";
+        model = "deepseek-r1:1.5b";
+        timeout = 30000;
+        temperature = 0;
+        max_completion_tokens = 8192;
+        reasoning_effort = "medium";
+      };
+    };
+  };
 }
