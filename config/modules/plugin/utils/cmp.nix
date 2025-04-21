65b69d6353a96699512c23643e981a0821183f86
André Augusto Bortoli <andre@bortoli.phd>
2025-04-21 20:20:51 -0300

    feat: add cmp keymaps

--- config/modules/plugin/utils/cmp.nix
+++ config/modules/plugin/utils/cmp.nix
@@ -2,10 +2,41 @@
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
-    settings.sources = [
-      { name = "nvim_lsp"; }
-      { name = "path"; }
-      { name = "buffer"; }
-    ];
+    settings = {
+      mapping.__raw = ''
+        cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.snippet.active({ direction = 1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(1)
+              end)
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.snippet.active({ direction = -1 }) then
+              vim.schedule(function()
+                vim.snippet.jump(-1)
+              end)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        })
+      '';
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+    };
   };
 }
